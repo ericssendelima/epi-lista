af9d6070ddcbba4235b5798e880017641f6d75cb
ericssendelima <ericssendelima@gmail.com>
2023-11-10 17:44:48 -0300

    "Elevação da tela de login e centralização da tela e link no cart"

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import CardsCart from "../../components/CardsCart/CardsCart";
 import { Link } from "react-router-dom";
 import { EpiListContext } from "../../context/EpiListContext";
